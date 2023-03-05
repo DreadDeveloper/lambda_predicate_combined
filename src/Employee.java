bac995568697890383b1723cdeaea61d29ba1d80
Abhinav Kumar <kumaraka2000@gmail.com>
2023-03-05 16:58:01 +0530

    Initial Commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee
+{
+	private int empId;
+	private String empName;
+	private float salary;
+	private short experience;
+	Employee(int empId, String empName, float salary, short experience)
+	{
+		this.empId = empId;
+		this.empName = empName;
+		this.salary = salary;
+		this.experience = experience;
+	}
+	public int getEmpId()
+	{
+		return empId;
+	}
+	public String getEmpName()
+	{
+		return empName;
+	}
+	public float getSalary()
+	{
+		return salary;
+	}
+	public short getExperience()
+	{
+		return experience;
+	}
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		ArrayList<Employee> employeeArrayList = new ArrayList<>();
+		Predicate<Employee> salaryPredicate= e -> (e.getSalary() > 50000.0f);
+		Predicate<Employee> experiencePredicate= e -> (e.getExperience() > (short)5);
+		employeeArrayList.add(new Employee(1, "Jeremy Clarkson", 150000.0f, (short)20));
+		employeeArrayList.add(new Employee(2, "Soviet Womble", 50000.0f, (short)7));
+		employeeArrayList.add(new Employee(3, "Donald Trump", 500000.0f, (short)30));
+		employeeArrayList.add(new Employee(4, "Me", 0.0f, (short)0));
+		employeeArrayList.add(new Employee(5, "You", -150000.0f, (short)-20));
+		for(Employee employee: employeeArrayList)
+		{
+			/*if(salaryPredicate.and(experiencePredicate).test(employee))
+			{
+				System.out.println(employee.getEmpId() + " " + employee.getEmpName() + " " + employee.getSalary() + " " + employee.getExperience());
+			}*/
+			/*if(salaryPredicate.and(experiencePredicate).test(employee))
+			{
+				System.out.println(employee.getEmpId() + " " + employee.getEmpName() + " " + employee.getSalary() + " " + employee.getExperience());
+			}*/
+			if(salaryPredicate.negate().test(employee))
+			{
+				System.out.println(employee.getEmpId() + " " + employee.getEmpName() + " " + employee.getSalary() + " " + employee.getExperience());
+			}
+		}
+	}
 }
